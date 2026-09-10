1532ed7b4c125b53caf411fccd2d1ba50aa0d555
sixtrees778899-stack <sixtrees778899@gmail.com>
2026-09-10 16:34:03 +1000

    Deploy LEGAVIK Plans guidance line break

--- src/ui/canonical-customer-links.js
+++ src/ui/canonical-customer-links.js
@@ -1,6 +1,6 @@
 export const APPROVED_TEST_ORIGIN='https://sixtrees778899-stack.github.io';
 export const APPROVED_TEST_BASE='/SKREK-auth-test';
-export const CURRENT_TEST_RELEASE='legavik-plans-intro-emphasis-20260910-1';
+export const CURRENT_TEST_RELEASE='legavik-plans-intro-linebreak-20260910-1';
 export const CURRENT_DEPLOYMENT_ID=CURRENT_TEST_RELEASE;
 
 const ROUTES=Object.freeze({
