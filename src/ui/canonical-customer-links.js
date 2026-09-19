d05ae1e240e0610ece9fe025cf881726721789ed
sixtrees778899-stack <sixtrees778899@gmail.com>
2026-09-19 22:08:17 +1000

    Deploy LEGAVIK plan to Recovery Map routing

--- src/ui/canonical-customer-links.js
+++ src/ui/canonical-customer-links.js
@@ -1,6 +1,6 @@
 export const APPROVED_TEST_ORIGIN='https://sixtrees778899-stack.github.io';
 export const APPROVED_TEST_BASE='/SKREK-auth-test';
-export const CURRENT_TEST_RELEASE='legavik-fresh-account-entry-20260919-1';
+export const CURRENT_TEST_RELEASE='legavik-plan-recovery-routing-rc-20260919-1';
 export const CURRENT_DEPLOYMENT_ID=CURRENT_TEST_RELEASE;
 
 const ROUTES=Object.freeze({
