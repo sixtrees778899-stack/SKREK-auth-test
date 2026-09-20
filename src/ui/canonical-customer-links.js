c4af7752a34d0b53885af09496d165b91d1d1da8
sixtrees778899-stack <sixtrees778899@gmail.com>
2026-09-20 23:20:54 +1000

    Deploy LEGAVIK Stage 1A resume RC

--- src/ui/canonical-customer-links.js
+++ src/ui/canonical-customer-links.js
@@ -1,6 +1,6 @@
 export const APPROVED_TEST_ORIGIN='https://sixtrees778899-stack.github.io';
 export const APPROVED_TEST_BASE='/SKREK-auth-test';
-export const CURRENT_TEST_RELEASE='legavik-signing-lifecycle-rc-20260920-1';
+export const CURRENT_TEST_RELEASE='legavik-stage1a-same-u1-resume-20260920-1';
 export const CURRENT_DEPLOYMENT_ID=CURRENT_TEST_RELEASE;
 
 const ROUTES=Object.freeze({
