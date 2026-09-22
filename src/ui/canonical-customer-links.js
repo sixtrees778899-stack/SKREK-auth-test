98ae1e9263cc4b0f16397ca0f6e86d9360bd4490
sixtrees778899-stack <sixtrees778899@gmail.com>
2026-09-22 12:03:44 +1000

    Deploy LEGAVIK Initial Creation Fresh Resume RC

--- src/ui/canonical-customer-links.js
+++ src/ui/canonical-customer-links.js
@@ -1,6 +1,6 @@
 export const APPROVED_TEST_ORIGIN='https://sixtrees778899-stack.github.io';
 export const APPROVED_TEST_BASE='/SKREK-auth-test';
-export const CURRENT_TEST_RELEASE='legavik-mainnet-quote-auto-refresh-20260922-1';
+export const CURRENT_TEST_RELEASE='legavik-initial-creation-fresh-resume-20260922-1';
 export const CURRENT_DEPLOYMENT_ID=CURRENT_TEST_RELEASE;
 
 const ROUTES=Object.freeze({
